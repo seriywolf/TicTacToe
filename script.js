8e0219ed311ef51d934261df6ffa20703a403172
Sergey Agafonov <seriywolf@gmail.com>
2022-10-13 20:44:05 +0700

    Add player func

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const gameboard = (() => {
-  new Array(9);
-  let arr = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'x', 'x'];
-  const render = (arr) => {
-    let elements = document.querySelectorAll(".boardElement");
-    elements.forEach(function(element,currentIndex) {
-        //console.log(arr[currentIndex])
-        element.textContent = arr[currentIndex];
-        //console.log(element.textContent);
-        //console.log(typeof currentIndex);
-        
-    });    
-
-
-  };
+  //let arr = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'x', 'x'];
+  let arr = new Array(9);
   return {
-    render,arr    
+    arr
   };
 })();
 
-const players = (name) => {};
+const player = (mark) =>{;
+  this.mark = mark
+  return {mark};  
+};
+
+const xPlayer = player("X");
+const oPlayer = player("O");
+
+const game = (() => {
+  const render = (arr) => {
+    let elements = document.querySelectorAll(".boardElement");
+    elements.forEach(function (element, currentIndex) {
+      element.textContent = arr[currentIndex];
+    });
+  };
+
+  document
+    .querySelector(".gameboard")
+    .addEventListener("click", function (event) {
+      if (event.target.textContent === "") {
+        //event.target.textContent = "!";
+        gameboard.arr[+event.target.classList[1]] = "!";
+        render(gameboard.arr);
+        //console.log(+event.target.classList[1]);
+      }
+    });
+})();
