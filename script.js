f7f3a4f3541ac5b42512b5014a7fb4f3361480c7
Sergey Agafonov <seriywolf@gmail.com>
2022-10-13 21:49:42 +0700

    Move render func to gameboard

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const gameboard = (() => {
   //let arr = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'x', 'x'];
   let arr = new Array(9);
+  const render = (arr) => {
+    let elements = document.querySelectorAll(".boardElement");
+    elements.forEach(function (element, currentIndex) {
+      element.textContent = arr[currentIndex];
+    });
+  };
   return {
     arr,
-  };  
+    render,
+  };
 })();
 
 const player = (mark) => {
   const win = () => {
     console.log(`Player ${mark} win!`);
   };
-  return { mark, win};
+  return { mark, win };
 };
 
 const xPlayer = player("X");
@@ -21,29 +28,20 @@ const randomPlayerSelect = () => {
   const rand = Math.floor(Math.random() * 2);
   return players[rand];
 };
-let playerMark= randomPlayerSelect();
-
+let playerMark = randomPlayerSelect();
 
 const game = (() => {
-  const render = (arr) => {
-    let elements = document.querySelectorAll(".boardElement");
-    elements.forEach(function (element, currentIndex) {
-      element.textContent = arr[currentIndex];
-    });
-    //console.log(arr);
-  };  
-
   document
     .querySelector(".gameboard")
     .addEventListener("click", function (event) {
-      if (event.target.textContent === "") { 
+      if (event.target.textContent === "") {
         if (playerMark === "X") {
-           playerMark = "0";          
+          playerMark = "0";
         } else if (playerMark === "0") {
-          playerMark = "X";          
-        };
+          playerMark = "X";
+        }
         gameboard.arr[+event.target.classList[1]] = playerMark;
-        render(gameboard.arr);
-      };
+        gameboard.render(gameboard.arr);
+      }
     });
 })();
