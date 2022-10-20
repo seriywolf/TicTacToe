018aeb8a6ed3a29c6daa3cbcd723ef1e9ce5bbc3
Sergey Agafonov <seriywolf@gmail.com>
2022-10-20 13:38:48 +0700

    Move winConditions inside game func

--- script.js
+++ script.js
@@ -83,16 +83,7 @@ const GetPlayer = (() => {
   };
 })();
 
-const winConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+
 
 const game = (() => {
   document.querySelector(".gameboard").addEventListener("click", (event) => {
@@ -110,6 +101,17 @@ const game = (() => {
     }
   });
 
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  
   document.querySelector("#restart").addEventListener("click", () => {
     gameboard.restart();
   });
